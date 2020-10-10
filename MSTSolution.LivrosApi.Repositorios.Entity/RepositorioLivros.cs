0a8dc6f56ca9d322f420d2023e457250b2527e4e
Matheus Rodrigues F da Silva <matheusuniversal@yahoo.com.br>
2020-10-10 16:53:39 -0300

    Desacoplando camada de acesso  a dados com repositorios

---
+++ MSTSolution.LivrosApi.Repositorios.Entity/RepositorioLivros.cs
@@ -0,0 +1,20 @@
+﻿using MSTSolution.LivrosApi.AcessoDados.Entity.Context;
+using MSTSolution.LivrosApi.Dominio;
+using MSTSolution.Repositorios.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTSolution.LivrosApi.Repositorios.Entity
+{
+    public class RepositorioLivros : RepositorioMSTSolution<Livro,int>
+    {
+        public RepositorioLivros(LivrosApiDbContext context)
+            :base (context)
+        {
+
+        }
+    }
+}
